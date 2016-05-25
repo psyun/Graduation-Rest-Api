25ab414f4a63b1dbbf4adac74568fec0c70bc537
pengshengyun <peng_shengyun@gowild.cn>
2016-05-25 23:08:14 +0800

    add Code First Test

---
+++ HDO2O.Models/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDO2O.Models
+{
+    public class Test
+    {
+        public Guid Id { get; set; }
+        public string name { get; set; }
+    }
+}
