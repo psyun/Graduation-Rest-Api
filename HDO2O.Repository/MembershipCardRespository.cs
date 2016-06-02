8767144a1ae04219e0a306165de4512c62281508
peng <cq_jiepng@163.com>
2016-06-02 23:21:23 +0800

    add membership

---
+++ HDO2O.Repository/MembershipCardRespository.cs
@@ -0,0 +1,20 @@
+﻿using HDO2O.Infranstructure;
+using HDO2O.IRepository;
+using HDO2O.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDO2O.Repository
+{
+    public class MembershipCardRespository : BaseRepository<MembershipCard, HDDbContext>, IMembershipCardRespository
+    {
+        public MembershipCardRespository(IDbContextFactory<HDDbContext> dbContextFactory)
+            : base(dbContextFactory)
+        {
+        }
+
+    }
+}
