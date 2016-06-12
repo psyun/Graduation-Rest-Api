0ea6cf85d6718bce4df36308d24980f627c90cc2
pengshengyun <peng_shengyun@gowild.cn>
2016-06-12 16:32:17 +0800

    add utils

---
+++ HDO2O.Utils/LocationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDO2O.Utils
+{
+    public class LocationModel
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+        public string locationTitle { get; set; }
+    }
+}
---
+++ HDO2O.Utils/MapUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDO2O.Utils
+{
+    public class MapUtil
+    {
+        public static double JudgeDistance(LocationModel sourceLocation, LocationModel destLocation)
+        {
+
+        }
+    }
+}
